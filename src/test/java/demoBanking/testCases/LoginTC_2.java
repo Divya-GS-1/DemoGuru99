2f175bff1be0abf117e02e10f230fca0f12d4b54
Divya G S <157267615+Divya-GS-1@users.noreply.github.com>
2024-02-05 21:24:56 +0530

    Commit

---
+++ src/test/java/demoBanking/testCases/LoginTC_2.java
@@ -0,0 +1,24 @@
+package demoBanking.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.Status;
+
+import demoBanking.pageObjects.LoginPage;
+
+
+public class LoginTC_2 extends BaseClass {
+
+	@Test
+	public void t1() {
+		System.out.println("T1 executed");
+	}
+	
+	@Test
+	public void t2() {
+		
+		System.out.println("T2 method executed");
+	}
+
+}
