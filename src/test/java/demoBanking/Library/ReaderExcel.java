ea4e87043df6419ba51117df887fc11837bfe492
Divya G S <157267615+Divya-GS-1@users.noreply.github.com>
2024-02-28 21:46:33 +0530

    encapsulation

---
+++ src/test/java/demoBanking/Library/ReaderExcel.java
@@ -0,0 +1,64 @@
+package demoBanking.Library;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReaderExcel {
+	XSSFSheet sheet;
+	File file;
+	XSSFWorkbook workbook;
+	
+	ReaderExcel(String file_path, String sheetName){
+		
+		try {
+			file=new File(file_path);
+			FileInputStream fis =new FileInputStream(file);
+			workbook=new XSSFWorkbook(fis);
+			sheet=workbook.getSheet(sheetName);
+		} catch (Exception e) {
+			System.out.println("Exceptions occured: "+ e.getMessage());
+		}
+	}
+	
+	public int getRowCount() {
+		return sheet.getLastRowNum();
+	}
+	
+	public int getLastColumn() {
+		return sheet.getRow(0).getLastCellNum();
+	}
+	
+	public String getCellData(int rowNum, int colNum) {
+		return sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
+	}
+	
+	public void setCellData(int rowNum, int colNum, String Data) {
+		XSSFRow row = sheet.getRow(rowNum);
+		if(row!=null) {
+			row.createCell(colNum).setCellValue(Data);
+		}else {
+			sheet.createRow(rowNum).createCell(colNum).setCellValue(Data);
+		}
+		
+		FileOutputStream fos;
+		try {
+			fos=new FileOutputStream(file);
+			workbook.write(fos);
+		} catch (Exception e) {
+			System.out.println("Exceptions occured: "+ e.getMessage());
+		}
+		fos.close();
+		workbook.close();
+	}
+	
+	
+	
+	
+}
